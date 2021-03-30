8d6d11b7bd94d1967f2c44d239658ac397c88bd4
JanHamara <jan@seculin.com>
2021-03-30 20:43:03 +0200

    develop/ commit header

--- wp-content/themes/chp/template-parts/header/page-header.php
+++ wp-content/themes/chp/template-parts/header/page-header.php
@@ -7,9 +7,16 @@
 ?>
 
 <header style="background: url('<?php echo get_template_directory_uri(); ?>/assets/src/img/header-bg.jpg')">
-    <div class="chp_header_content">
+    <div class="chp_header_content flex column main-center cross-center">
         <div class="container">
-            Hello Friend!!!
+            <h1 class="chp_header_title">Chirurgie de la paupière</h1>
+
+            <h2 class="chp_header_subtitle">Centre de chirurgie de l'orbite, des paupières et des voies lacrymales</h2>
+
+            <div class="chp_header_links flex row main-start">
+                <a href="javascript:void(0)" class="chp_button chp_button_light">Découvrez nos médecins <i class="fas fa-chevron-down"></i></a>
+                <a href="javascript:void(0)" class="chp_button chp_button_light">Pathologies &amp; traitements <i class="fas fa-chevron-down"></i></a>
+            </div>
         </div>
     </div>
 </header>
