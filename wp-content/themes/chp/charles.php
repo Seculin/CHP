c2b22d0505ec464d2702b87390adbe66c700b8bc
JanHamara <jan@seculin.com>
2021-04-01 15:34:10 +0200

    develop/ commit clinique header

--- wp-content/themes/chp/charles.php
+++ wp-content/themes/chp/charles.php
@@ -12,9 +12,17 @@
 <main>
 
     <section class="chp_header chp_h_cliniques">
-        <div class="chp_container flex column main-center cross-center">
-            <div class="container">
-                <h3>Charles</h3>
+        <div class="chp_container chp_cliniques flex column main-center cross-center">
+        <div class="container">
+                <div class="chp_cliniques_image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/clinic2.jpg" class="nav_img" alt="clinic-photo-thumbnail">
+                </div>
+
+                <h1>Clinique Saint-Charles</h1>
+
+                <h2>Créé en 1909 par les Soeurs de Saint Charles de Lyon, c’est un établissement certifié par la Haute Autorité de Santé</h2>
+
+                <a href="#chp_charles" class="chp_button chp_button_dark">En savoir plus <i class="fas fa-chevron-down"></i></a>
             </div>
         </div>
     </section>
--- wp-content/themes/chp/sauvegarde.php
+++ wp-content/themes/chp/sauvegarde.php
@@ -12,9 +12,17 @@
 <main>
 
     <section class="chp_header chp_h_cliniques">
-        <div class="chp_container flex column main-center cross-center">
+        <div class="chp_container chp_cliniques flex column main-center cross-center">
             <div class="container">
-                <h3>Sauvegarde</h3>
+                <div class="chp_cliniques_image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/clinic1.jpg" class="nav_img" alt="clinic-photo-thumbnail">
+                </div>
+
+                <h1>Clinique de la Sauvegarde</h1>
+
+                <h2>La clinique de la Sauvegarde est un établissement conventionné certifié par la Haute Autorité de Santé</h2>
+
+                <a href="#chp_sauvegarde" class="chp_button chp_button_dark">En savoir plus <i class="fas fa-chevron-down"></i></a>
             </div>
         </div>
     </section>
